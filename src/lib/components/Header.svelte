b68fd2d09cefb4bfa2aa579045eb79a9a729ab51
Henil Malaviya <henilm.dev@gmail.com>
2025-04-20 10:25:46 +0530

    chore: dummy channels layout

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,9 @@
 <div class="flex items-center justify-between px-4 py-4">
 	<div class="flex items-center justify-start gap-2">
 		<img src="/favicon.svg" alt="logo" class="h-12 w-12" />
-		<span class="text-2xl font-bold">NoOne</span>
+		<div class="flex flex-col gap-0">
+			<span class="text-2xl font-bold">NoOne</span>
+			<span class="text-xs">NoOne will never know</span>
+		</div>
 	</div>
 </div>
